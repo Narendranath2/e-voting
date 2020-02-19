0e0275909e95a9911cb81144f66424909fbe5da4
Narendranath2 <narendranathgoparaju@gmail.com>
2020-02-19 17:29:24 +0530

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Login';
 import Signup from './Signup';
 import Vote from './Vote';
