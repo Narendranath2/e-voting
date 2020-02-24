49e42119a7e4a142e02554a7c184dda8557b1abc
Narendranath2 <narendranathgoparaju@gmail.com>
2020-02-24 14:10:11 +0530

    fixed the router

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import Navi from './Navi';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  
+  //<Route path={`${process.env.PUBLIC_URL}/`} exact component={Home}></Route>
   return (
       <Router>
         <Navi></Navi>
         <Switch>
-          <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home}></Route>
+          <Route path='/' exact component={Home}></Route>
           <Route path='/login' component={Login}></Route>
           <Route path='/signup' component={Signup}></Route>
           <Route path='/vote' component={Vote}></Route>
